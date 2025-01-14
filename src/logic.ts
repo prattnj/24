a45b1a1ab5b9b295444a624d4b7de573573f1998
Noah Pratt <prattnj@gmail.com>
2025-01-13 22:22:42 -0700

    Create stub of logic functions

---
+++ src/logic.ts
@@ -0,0 +1,14 @@
+function generatePuzzle(): number[] {
+    // continually generates 4 digits until they have a solution
+    return [];
+}
+
+function generateRandomDigit(): number {
+    // generate a 0-9
+    return 0;
+}
+
+function digitsHaveSolution(digits: number[]): boolean {
+    // determines if the 4 operands can be manipulated with basic arithmetic operations (+, -, x, /) to equal 24
+    return false;
+}
