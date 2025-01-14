2f05300b731ef0cf3b48ec01bf4761338924e656
Noah Pratt <prattnj@gmail.com>
2025-01-14 00:06:45 -0700

    Add all (easy) logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import { generatePuzzle } from './logic.ts';
 
 export const App = () => {
+  const [digits, setDigits] = useState<number[]>([]);
+
   return (
     <div class='text-gray-200 w-full f-col items-center'>
       <div class='p-4 text-6xl'>24</div>
-      <div class='button'>Click to generate numbers...</div>
-      <div class='f-row mt-4'>
-        <div class='number mr-4'>1</div>
-        <div class='number mr-4'>2</div>
-        <div class='number mr-4'>3</div>
-        <div class='number'>4</div>
+      <button class='button' onClick={() => setDigits(generatePuzzle())}>Click to generate numbers...</button>
+      <div class='f-col mt-4'>
+        <div class='f-row'>
+          <div class='number mr-4'>{digits[0] || ' '}</div>
+          <div class='number'>{digits[1] || ' '}</div>
+        </div>
+        <div class='f-row mt-4'>
+          <div class='number mr-4'>{digits[2] || ' '}</div>
+          <div class='number'>{digits[3] || ' '}</div>
+        </div>
       </div>
     </div>
   );
--- src/logic.ts
+++ src/logic.ts
@@ -1,14 +1,20 @@
-function generatePuzzle(): number[] {
+export function generatePuzzle(): number[] {
     // continually generates 4 digits until they have a solution
-    return [];
+    let hasSolution = false;
+    let digits: number[] = [];
+    do {
+        digits = [generateRandomDigit(), generateRandomDigit(), generateRandomDigit(), generateRandomDigit()];
+        hasSolution = digitsHaveSolution(digits);
+    } while (!hasSolution);
+    return digits;
 }
 
 function generateRandomDigit(): number {
     // generate a 0-9
-    return 0;
+    return Math.floor(Math.random() * 9) + 1;
 }
 
 function digitsHaveSolution(digits: number[]): boolean {
     // determines if the 4 operands can be manipulated with basic arithmetic operations (+, -, x, /) to equal 24
-    return false;
+    return true;
 }
