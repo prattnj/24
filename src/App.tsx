35f13bf5161fbb4f3dc57a470b5d0d708ca1f759
Noah Pratt <noahp@DHICorp.com>
2025-01-21 20:33:46 -0700

    Fix console warning and log solutions

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,21 @@ export const App = () => {
   const [digits, setDigits] = useState<number[]>([]);
 
   return (
-    <div class='text-gray-200 w-full f-col items-center'>
-      <div class='p-4 text-6xl'>24</div>
-      <button class='button' onClick={() => setDigits(generatePuzzle())}>Click to generate numbers...</button>
-      <div class='f-col mt-4'>
-        <div class='f-row'>
-          <div class='number mr-4'>{digits[0] || ' '}</div>
-          <div class='number'>{digits[1] || ' '}</div>
+    <div className='text-gray-200 w-full f-col items-center'>
+      <div className='p-4 text-6xl'>24</div>
+      <button className='button' onClick={() => {
+        const [numbers, solutions] = generatePuzzle();
+        setDigits(numbers);
+        console.log(solutions);
+      }}>Click to generate numbers...</button>
+      <div className='f-col mt-4'>
+        <div className='f-row'>
+          <div className='number mr-4'>{digits[0] || ' '}</div>
+          <div className='number'>{digits[1] || ' '}</div>
         </div>
-        <div class='f-row mt-4'>
-          <div class='number mr-4'>{digits[2] || ' '}</div>
-          <div class='number'>{digits[3] || ' '}</div>
+        <div className='f-row mt-4'>
+          <div className='number mr-4'>{digits[2] || ' '}</div>
+          <div className='number'>{digits[3] || ' '}</div>
         </div>
       </div>
     </div>
